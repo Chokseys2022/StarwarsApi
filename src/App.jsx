c0363c7613561bb3afba7ad34537e6312d88f9e8
Chokseys2022 <choksey2022@gmail.com>
2024-03-09 09:53:30 -0800

    starshipcard file created

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import StarshipCard from "./StarshipCard";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [starships, setStarships] = useState(0);
+
+  useEffect(() => {
+    const fetchStarships = async () => {
+      try {
+        const data = await getAllStartships();
+        setStarships(data.results);
+      } catch (error) {
+        console.error("Error fetching startship data", error);
+      }
+    };
+
+    fetchStarships();
+  }, []);
 
   return (
-    <>
-     <h1>Hello</h1>
-    </>
-  )
+    <div className="App">
+      {starships.map((starship) => (
+        <StarshipCard key={starship.name} name={starship.name} />
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/StarshipCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function StarshipCard() {
+  return (
+    <div>StarshipCard</div>
+  )
+}
+
+export default StarshipCard
